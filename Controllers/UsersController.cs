fca617bb3c46d1269cfd3826e1b904766f57db6f
Julian Rivera <julianarevircoins@gmail.com>
2023-11-25 23:40:00 -0500

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Users.Controllers;
+
+public class UsersController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+    public string WelcomeNot(string name, int numTimes = 1)
+{
+    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+}
+}
---
+++ Data/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication2.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication2
+{
+public class DataBaseContext : DbContext
+{
+    public DataBaseContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<UsersModel> Users{get;set;}
+}
+
+}
